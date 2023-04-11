aad6a18c91438a4ba61b8e119a56c913dce4bc90
V Udayani <vudayani@vudayaniSMD6M.vmware.com>
2023-04-11 11:14:34 +0530

    Initial commit for Tanzu Java Native Web App

---
+++ src/main/java/com/example/demo/HelloNativeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloNativeApplication.class, args);
+	}
+
+}
