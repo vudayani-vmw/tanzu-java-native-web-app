32f285e4fddf34ef5b55ddb336e06affb767e5e0
V Udayani <vudayani@vudayaniSMD6M.vmware.com>
2023-04-12 09:50:11 +0530

    add controller

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot + Tanzu!";
+	}
+
+	@RequestMapping("/hello")
+	public String test1() {
+		return "Greetings!!!!";
+	}
+    
+}
