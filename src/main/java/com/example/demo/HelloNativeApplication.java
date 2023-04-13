ab900f61b38f8ac076fa8afef84e1385300dc90b
V Udayani <vudayani@vudayaniSMD6M.vmware.com>
2023-04-13 10:31:00 +0530

    add htttpTraceRepository bean

--- src/main/java/com/example/demo/HelloNativeApplication.java
+++ src/main/java/com/example/demo/HelloNativeApplication.java
@@ -1,7 +1,10 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HelloNativeApplication {
@@ -9,5 +12,10 @@ public class HelloNativeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloNativeApplication.class, args);
 	}
+
+	@Bean
+    public HttpTraceRepository htttpTraceRepository() {
+        return new InMemoryHttpTraceRepository();
+    };
 
 }
